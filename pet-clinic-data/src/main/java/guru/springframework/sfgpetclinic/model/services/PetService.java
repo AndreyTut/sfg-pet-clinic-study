46f32a334303c9013b055bb809fdfb874fa1ed4a
Andrey <andreww@ukr.net>
2020-07-12 15:00:37 +0300

    common service interface added

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/PetService.java
@@ -1,14 +1,6 @@
 package guru.springframework.sfgpetclinic.model.services;
 
-import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Owner, Long> {
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/VetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/services/VetService.java
@@ -1,14 +1,6 @@
 package guru.springframework.sfgpetclinic.model.services;
 
-import guru.springframework.sfgpetclinic.model.Vet;
+import guru.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Owner, Long> {
 }
