78642b54bd730eebbbd9d8b2cdbc8a59a9c8d6b7
Andrey <andreww@ukr.net>
2020-08-10 18:26:31 +0300

    added SpecialityMapService

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.map.AbstractMapService;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
