6267f7b8ecf741f09b597a19848a37c855136db9
Andrey <andreww@ukr.net>
2020-08-17 11:41:32 +0300

    JPA repositories added, Owner spring data jpa service added

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/Speciality.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface Speciality extends CrudRepository<Speciality, Long> {
+}
